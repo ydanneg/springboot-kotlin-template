11baade17110f983ecaa03760ed494c030aee423
Gennadi Kudrjavtsev <gennadi@agrello.org>
2023-11-04 23:38:22 +0200

    use main method for springboot test context initialization

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(myLibs.plugins.springboot)
     alias(myLibs.plugins.kotlin.serialization)
     alias(myLibs.plugins.kotlin.spring)
-    alias(myLibs.plugins.graalvm)
+//    alias(myLibs.plugins.graalvm)
     application
 }
 
@@ -22,7 +22,6 @@ dependencies {
     implementation(myLibs.springboot.starter.webflux)
     implementation(myLibs.springboot.starter.validation)
     developmentOnly("${myLibs.springboot.devtools.get().module}:${springBom.version}")
-//    kapt("${documentLibs.springboot.configuration.processor.get().module}:${springBom.version}")
     implementation(myLibs.springboot.autoconfigure)
     implementation(myLibs.springdoc.openapi.webflux.ui)
 
--- service/src/test/kotlin/com/ydanneg/springboot/test/SpringBootIntegrationTest.kt
+++ service/src/test/kotlin/com/ydanneg/springboot/test/SpringBootIntegrationTest.kt
@@ -2,12 +2,13 @@ package com.ydanneg.springboot.test
 
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.UseMainMethod.ALWAYS
 import java.lang.annotation.Inherited
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Inherited
-@SpringBootTest
+@SpringBootTest(useMainMethod = ALWAYS)
 @AutoConfigureWebTestClient
 annotation class SpringBootIntegrationTest {
 }
